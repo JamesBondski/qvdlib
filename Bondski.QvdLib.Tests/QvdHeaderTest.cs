a4c15b2a5f9dbe1d49ec6443f0f7506bbc8dfda2
Matthias Kersting <aldart@gmail.com>
2021-04-13 20:48:44 +0200

    Added QvdHeader class and initialization.

---
+++ Bondski.QvdLib.Tests/QvdHeaderTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml.Linq;
+using Xunit;
+
+namespace Bondski.QvdLib.Tests
+{
+    public class QvdHeaderTest
+    {
+        private string invalidDocXml = @"<TableHeader></TableHeader>";
+        private XDocument invalidDoc => XDocument.Parse(this.invalidDocXml);
+
+        [Fact]
+        public void Constructor_XDocNotQvdTableHeader()
+        {
+            _ = Assert.Throws<ArgumentException>(() => { QvdHeader header = new QvdHeader(this.invalidDoc); });
+        }
+
+        [Fact]
+        public void Constructor_XElementNotQvdTableHeader()
+        {
+            _ = Assert.Throws<ArgumentException>(() => { QvdHeader header = new QvdHeader(this.invalidDoc.Root); });
+        }
+    }
+}
---
+++ Bondski.QvdLib/QvdHeader.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="QvdHeader.cs" company="Matthias Kersting">
+// Copyright (c) Matthias Kersting. All rights reserved.
+// </copyright>
+
+namespace Bondski.QvdLib
+{
+    using System;
+    using System.Xml.Linq;
+
+    /// <summary>
+    /// Holds the information from a QvdTableHeader element.
+    /// </summary>
+    public class QvdHeader
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QvdHeader"/> class.
+        /// </summary>
+        /// <param name="qvdTableHeaderDoc">XDocument containing the QvdTableHeader element.</param>
+        public QvdHeader(XDocument qvdTableHeaderDoc)
+            : this(qvdTableHeaderDoc.Root)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QvdHeader"/> class.
+        /// </summary>
+        /// <param name="qvdTableHaderElement">QvdTableHeader element.</param>
+        public QvdHeader(XElement qvdTableHaderElement)
+        {
+            if (qvdTableHaderElement.Name != "QvdTableHeader")
+            {
+                throw new ArgumentException("XML element not QvdTableHeader.");
+            }
+
+            this.QvdTableHeaderElement = qvdTableHaderElement;
+        }
+
+        /// <summary>
+        /// Gets the XML element containing all header information.
+        /// </summary>
+        public XElement QvdTableHeaderElement { get; }
+    }
+}
