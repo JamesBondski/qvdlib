97fa01634df230d226b8031303ae2dbcc16abed2
Matthias Kersting <aldart@gmail.com>
2021-06-12 23:14:58 +0200

    Basic test cases for reading field info.

--- Bondski.QvdLib.Tests/QvdHeaderTest.cs
+++ Bondski.QvdLib.Tests/QvdHeaderTest.cs
@@ -29,5 +29,27 @@ namespace Bondski.QvdLib.Tests
         {
             _ = Assert.Throws<InvalidHeaderException>(() => { QvdHeader header = new QvdHeader(this.docWithoutFields.Root); } );
         }
+
+        private string fieldNameXml = @"<QvdTableHeader><Fields><QvdFieldHeader><FieldName>Test</FieldName></QvdFieldHeader></Fields></QvdTableHeader>";
+        private XDocument docFieldName => XDocument.Parse(this.fieldNameXml);
+
+        private XDocument GetDoc(string xml)
+        {
+            return XDocument.Parse(xml);
+        }
+
+        [Fact]
+        public void Fields_FieldName()
+        {
+            var header = new QvdHeader(this.docFieldName);
+            Assert.Equal("Test", header.Fields[0].name);
+        }
+
+        [Fact]
+        public void Fields_ThrowsIfNoName()
+        {
+            string xml = @"<QvdTableHeader><Fields><QvdFieldHeader></QvdFieldHeader></Fields></QvdTableHeader>";
+            _ = Assert.Throws<InvalidHeaderException>(() => { QvdHeader header = new QvdHeader(GetDoc(xml)); });
+        }
     }
 }
